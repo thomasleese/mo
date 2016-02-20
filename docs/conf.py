1c6fe55f32b7335d92b51cffce21450af84d3170
Thomas Leese <inbox@thomasleese.me>
2016-02-20 10:55:11 +0000

    Add initial docs configuration

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+import sys
+import os
+
+from mo import __version__
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = 'M-O'
+copyright = '2016, Thomas Leese'
+author = 'Thomas Leese'
+
+version = __version__
+release = __version__
+
+language = None
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+todo_include_todos = False
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+
+htmlhelp_basename = 'M-Odoc'
+
+intersphinx_mapping = {'https://docs.python.org/': None}
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. M-O documentation master file, created by
+   sphinx-quickstart on Sat Feb 20 10:49:03 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to M-O's documentation!
+===============================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
