06cb09583dbbbc5f747f57ee3f2dbbd5eb9b5e45
Thomas Leese <thomas@leese.io>
2017-10-27 07:25:23 +0100

    Move steps into a separate file

--- mo/runner.py
+++ mo/runner.py
@@ -1,8 +1,4 @@
-from collections import namedtuple
-import select
-import subprocess
-
-from . import events
+from . import events, steps
 from .project import NoSuchTaskError, Step
 
 
@@ -64,69 +60,6 @@ class Runner:
 
         return values
 
-    def run_command_step(self, task, step, variables):
-        """Run a command step."""
-
-        command = step.args.format(**variables)
-
-        yield events.running_command(command)
-
-        process = subprocess.Popen(command, shell=True,
-                                   universal_newlines=True, bufsize=1,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-
-        while True:
-            reads = [process.stdout.fileno(), process.stderr.fileno()]
-            ret = select.select(reads, [], [])
-
-            for fd in ret[0]:
-                if fd == process.stdout.fileno():
-                    line = process.stdout.readline().strip()
-                    if line:
-                        yield events.command_output('stdout', line)
-                if fd == process.stderr.fileno():
-                    line = process.stderr.readline().strip()
-                    if line:
-                        yield events.command_output('stderr', line)
-
-            if process.poll() != None:
-                break
-
-        for line in process.stdout.readlines():
-            line = line.strip()
-            if line:
-                yield events.command_output('stdout', line)
-
-        for line in process.stderr.readlines():
-            line = line.strip()
-            if line:
-                yield events.command_output('stderr', line)
-
-        if process.returncode != 0:
-            yield events.command_failed(process.returncode)
-            raise StopTask
-
-    def run_help_step(self, task, step, variables):
-        """Run a help step."""
-
-        task_name = step.args or variables['task']
-
-        try:
-            task = self.project.find_task(task_name)
-        except NoSuchTaskError as e:
-            yield events.task_not_found(task_name, e.similarities)
-            raise StopTask
-
-        text = '# {}\n'.format(task.name)
-        text += '\n'
-        text += task.description
-        text += '\n\n'
-        text += 'Variables: {}' \
-            .format(', '.join(task.variables))
-
-        yield events.help_step_output(text)
-
     def run_task(self, name):
         """Run a task."""
 
@@ -159,12 +92,12 @@ class Runner:
                     yield events.undefined_variable_error(e.args[0])
                     raise StopTask
 
-                if step.type == 'help':
-                    yield from self.run_help_step(task, step, variables)
-                elif step.type == 'command':
-                    yield from self.run_command_step(task, step, variables)
-                else:
+                try:
+                    step_function = getattr(steps, step.type)
+                except AttributeError:
                     yield events.unknown_step_type_error(step)
                     raise StopTask
+                else:
+                    yield from step_function(self.project, task, step, variables)
 
             yield events.finished_task(task)
